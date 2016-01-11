38896143dc5c87dc44438728f6f07365bebaad30
temka1234 <art.ermolin@gmail.com>
2016-01-11 04:20:28 +0300

    Change redirectTo usage

--- dist/angular-permission.js
+++ dist/angular-permission.js
@@ -17,37 +17,41 @@
         return state.data && state.data.permissions ? state.data.permissions : null;
       }
 
-      $transitions.onStart({ 
-        to: function (state) {
-          return getPermissions(state);
-        }
-      }, 
-      function ($transition$, $state) {
-        var toState = $transition$.to();
-        var permissions = getPermissions(toState);
-        var toParams = $transition$.params();
+      $transitions.onStart(
+        {
+          to: function (state) {
+            return getPermissions(state);
+          }
+        }, 
+        function ($transition$, $state) {
+          var toState = $transition$.to();
+          var permissions = getPermissions(toState);
+          var toParams = $transition$.params();
 
-        $rootScope.$broadcast('$stateChangePermissionStart', toState, toParams);
-        return Permission.authorize(permissions, toParams)
-          .then(function () {
-            $rootScope.$broadcast('$stateChangePermissionAccepted', toState, toParams)
-          })
-          .catch(function (error) {
-            $rootScope.$broadcast('$stateChangePermissionDenied', toState, toParams);
-            if (permissions.redirectTo) {
-              var redirectTo = permissions.redirectTo;
-              if (angular.isFunction(redirectTo)) {
-                redirectTo = redirectTo();
+          if ($rootScope.$broadcast('$stateChangePermissionStart', toState, toParams).defaultPrevented) {
+            return false;
+          }
+          return Permission.authorize(permissions, toParams)
+            .then(function () {
+              $rootScope.$broadcast('$stateChangePermissionAccepted', toState, toParams)
+            })
+            .catch(function (error) {
+              $rootScope.$broadcast('$stateChangePermissionDenied', toState, toParams);
+              if (permissions.redirectTo) {
+                var redirectTo = permissions.redirectTo;
+                if (angular.isFunction(redirectTo)) {
+                  redirectTo = redirectTo();
+                }
+                return $q.when(redirectTo).then(function (redirectState) {
+                  var targetState = $state.target(redirectState, toParams);
+                  return targetState;
+                });
+              } else {
+                return false;
               }
-              return $q.when(permissions.redirectTo, function (redirectState) {
-                var targetState = $state.target(redirectState);
-                return targetState;
-              });
-            } else {
-              return false;
-            }
-          })
-      })
+            })
+        }
+      )
     }]);
 }());
 (function () {
