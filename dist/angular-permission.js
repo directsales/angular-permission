ccf5a5e33a1b6c1f825902a5caedd8df4691836e
temka1234 <art.ermolin@gmail.com>
2016-01-11 04:21:13 +0300

    Revert "Change redirectTo usage"
    This reverts commit ff132c930951a3fa1d9f966d3b077d25a7289142.

--- dist/angular-permission.js
+++ dist/angular-permission.js
@@ -17,41 +17,37 @@
         return state.data && state.data.permissions ? state.data.permissions : null;
       }
 
-      $transitions.onStart(
-        {
-          to: function (state) {
-            return getPermissions(state);
-          }
-        }, 
-        function ($transition$, $state) {
-          var toState = $transition$.to();
-          var permissions = getPermissions(toState);
-          var toParams = $transition$.params();
-
-          if ($rootScope.$broadcast('$stateChangePermissionStart', toState, toParams).defaultPrevented) {
-            return false;
-          }
-          return Permission.authorize(permissions, toParams)
-            .then(function () {
-              $rootScope.$broadcast('$stateChangePermissionAccepted', toState, toParams)
-            })
-            .catch(function (error) {
-              $rootScope.$broadcast('$stateChangePermissionDenied', toState, toParams);
-              if (permissions.redirectTo) {
-                var redirectTo = permissions.redirectTo;
-                if (angular.isFunction(redirectTo)) {
-                  redirectTo = redirectTo();
-                }
-                return $q.when(redirectTo).then(function (redirectState) {
-                  var targetState = $state.target(redirectState, toParams);
-                  return targetState;
-                });
-              } else {
-                return false;
-              }
-            })
+      $transitions.onStart({ 
+        to: function (state) {
+          return getPermissions(state);
         }
-      )
+      }, 
+      function ($transition$, $state) {
+        var toState = $transition$.to();
+        var permissions = getPermissions(toState);
+        var toParams = $transition$.params();
+
+        $rootScope.$broadcast('$stateChangePermissionStart', toState, toParams);
+        return Permission.authorize(permissions, toParams)
+          .then(function () {
+            $rootScope.$broadcast('$stateChangePermissionAccepted', toState, toParams)
+          })
+          .catch(function (error) {
+            $rootScope.$broadcast('$stateChangePermissionDenied', toState, toParams);
+            if (permissions.redirectTo) {
+              var redirectTo = permissions.redirectTo;
+              if (angular.isFunction(redirectTo)) {
+                redirectTo = redirectTo();
+              }
+              return $q.when(permissions.redirectTo, function (redirectState) {
+                var targetState = $state.target(redirectState);
+                return targetState;
+              });
+            } else {
+              return false;
+            }
+          })
+      })
     }]);
 }());
 (function () {
